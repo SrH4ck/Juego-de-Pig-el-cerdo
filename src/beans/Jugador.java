fd1d5c1c3008bd543f5fb65e396a38ee5f0fe87b
SrH4ck <antonio.iesgerena@gmail.com>
2024-06-30 15:50:37 +0200

    Actualizar Jugador.java
    actualización de la clase Jugador

--- src/beans/Jugador.java
+++ src/beans/Jugador.java
@@ -1,10 +1,11 @@
 package beans;
 
 public class Jugador {
-    private int puntos= 0;
+    private int puntos;
 
     public int tirarDado(Dado dado){
         return dado.tirada();
+        this.puntos = 0;
     }
     public void apuntarPuntos(int tirada){
         puntos +=tirada;
